8e4c77918b78512c20dbc2ba564f552769755d46
obairka <obairka@users.noreply.github.com>
2024-03-16 12:07:55 +0000

    deploy: 2mba/landingpage@25ef92cea3d23aa5a3623bf213c1d50366abe334

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"index.html": "3a9754e2bacf7d09496e29c9eb96b2f8",
-"/": "3a9754e2bacf7d09496e29c9eb96b2f8",
+const RESOURCES = {"index.html": "8bf281342de44584325fa2cf98d89d33",
+"/": "8bf281342de44584325fa2cf98d89d33",
 "canvaskit/canvaskit.js": "c86fbd9e7b17accae76e5ad116583dc4",
 "canvaskit/skwasm.js": "445e9e400085faead4493be2224d95aa",
 "canvaskit/skwasm.wasm": "e42815763c5d05bba43f9d0337fa7d84",
